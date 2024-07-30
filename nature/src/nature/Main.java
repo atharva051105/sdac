8d59960e30104d780eae23fa33fa2a577146daf0
patha <patha@LAPTOP-U0EQITT4>
2024-07-30 08:23:39 +0530

    sdac4

--- nature/src/nature/Main.java
+++ nature/src/nature/Main.java
@@ -2,6 +2,7 @@ package nature;
 
 public class Main {
 public static void main(String[] args) {
-	System.out.println("nature  3");
+	System.out.println("nature ");
+	System.out.println("sdac");
 }
 }
